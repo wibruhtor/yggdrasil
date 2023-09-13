5e7b0c7ea62a66c689ed09a5a6b47f97a51dcf8f
BruhaBruh <drugsho.jaker@gmail.com>
2023-09-13 12:51:52 +0500

    feat: add config with dotenv support

---
+++ src/config.rs
@@ -0,0 +1,45 @@
+use std::{
+    env::{self, VarError},
+    error::Error,
+};
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub http: Http,
+}
+
+impl Config {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        dotenvy::dotenv()?;
+
+        Ok(Config { http: Http::new() })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Http {
+    pub host: String,
+    pub port: String,
+}
+
+impl Http {
+    fn new() -> Self {
+        Http {
+            host: env::var("HOST").or_default("0.0.0.0".to_owned()),
+            port: env::var("PORT").or_default("3000".to_owned()),
+        }
+    }
+}
+
+trait OrDefault {
+    fn or_default(&self, default: String) -> String;
+}
+
+impl OrDefault for Result<String, VarError> {
+    fn or_default(&self, default: String) -> String {
+        match self {
+            Ok(v) => v.to_owned(),
+            Err(_) => default,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod config;
+
+// #[tokio::main]
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = config::Config::new()?;
+
+    println!("{:#?}", config);
+
+    Ok(())
 }
