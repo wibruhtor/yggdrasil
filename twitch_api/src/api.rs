8c80a3c471c4773548f54f9c5cc9db96a5ddd256
BruhaBruh <drugsho.jaker@gmail.com>
2023-11-22 19:40:17 +0500

    fix: unauthorized after time

--- twitch_api/src/api.rs
+++ twitch_api/src/api.rs
@@ -3,7 +3,7 @@ use std::ops::Add;
 
 use axum::http::StatusCode;
 use chrono::{Duration, Utc};
-use reqwest::{Client, RequestBuilder, Response, Url};
+use reqwest::{Client, Error, RequestBuilder, Response, Url};
 use serde::de::DeserializeOwned;
 use tokio::sync::RwLock;
 use tracing::instrument;
@@ -49,9 +49,7 @@ impl TwitchApi {
 
         let request = self.request("/users", Some(query_params)).await?;
 
-        let response = request
-            .send()
-            .await
+        let mut response = self.send_with_retry_on_unauthorized(request).await
             .map_err(|e| TwitchApi::FAIL_GET_USER_INFO_ERROR.clone().cause(e.into()))?;
 
         if !response.status().is_success() {
@@ -75,11 +73,12 @@ impl TwitchApi {
     pub async fn get_global_emotes(&self) -> AppResult<Vec<Emote>> {
         let request = self.request("/chat/emotes/global", None).await?;
 
-        let response = request.send().await.map_err(|e| {
-            TwitchApi::FAIL_GET_GLOBAL_EMOTES_ERROR
-                .clone()
-                .cause(e.into())
-        })?;
+        let response = self.send_with_retry_on_unauthorized(request).await
+            .map_err(|e| {
+                TwitchApi::FAIL_GET_GLOBAL_EMOTES_ERROR
+                    .clone()
+                    .cause(e.into())
+            })?;
 
         if !response.status().is_success() {
             return Err(TwitchApi::FAIL_REQUEST_WITH_STATUS_CODE_ERROR
@@ -103,11 +102,12 @@ impl TwitchApi {
 
         let request = self.request("/chat/emotes", Some(query_params)).await?;
 
-        let response = request.send().await.map_err(|e| {
-            TwitchApi::FAIL_GET_CHANNEL_EMOTES_ERROR
-                .clone()
-                .cause(e.into())
-        })?;
+        let response = self.send_with_retry_on_unauthorized(request).await
+            .map_err(|e| {
+                TwitchApi::FAIL_GET_CHANNEL_EMOTES_ERROR
+                    .clone()
+                    .cause(e.into())
+            })?;
 
         if !response.status().is_success() {
             return Err(TwitchApi::FAIL_REQUEST_WITH_STATUS_CODE_ERROR
@@ -129,11 +129,12 @@ impl TwitchApi {
     pub async fn get_global_badges(&self) -> AppResult<Vec<Badge>> {
         let request = self.request("/chat/badges/global", None).await?;
 
-        let response = request.send().await.map_err(|e| {
-            TwitchApi::FAIL_GET_GLOBAL_BADGES_ERROR
-                .clone()
-                .cause(e.into())
-        })?;
+        let response = self.send_with_retry_on_unauthorized(request).await
+            .map_err(|e| {
+                TwitchApi::FAIL_GET_GLOBAL_BADGES_ERROR
+                    .clone()
+                    .cause(e.into())
+            })?;
 
         if !response.status().is_success() {
             return Err(TwitchApi::FAIL_REQUEST_WITH_STATUS_CODE_ERROR
@@ -157,11 +158,12 @@ impl TwitchApi {
 
         let request = self.request("/chat/badges", Some(query_params)).await?;
 
-        let response = request.send().await.map_err(|e| {
-            TwitchApi::FAIL_GET_CHANNEL_BADGES_ERROR
-                .clone()
-                .cause(e.into())
-        })?;
+        let response = self.send_with_retry_on_unauthorized(request).await
+            .map_err(|e| {
+                TwitchApi::FAIL_GET_CHANNEL_BADGES_ERROR
+                    .clone()
+                    .cause(e.into())
+            })?;
 
         if !response.status().is_success() {
             return Err(TwitchApi::FAIL_REQUEST_WITH_STATUS_CODE_ERROR
@@ -203,9 +205,7 @@ impl TwitchApi {
 
         let request = client.post(TOKEN_URL).form(&form);
 
-        let response = request
-            .send()
-            .await
+        let response = self.send_with_retry_on_unauthorized(request).await
             .map_err(|e| TwitchApi::FAIL_GET_USER_TOKEN_ERROR.clone().cause(e.into()))?;
 
         if !response.status().is_success() {
@@ -258,7 +258,12 @@ impl TwitchApi {
             let token = app_token.token().ok_or(AppError::UNEXPECTED)?;
             return Ok(token);
         }
-        drop(app_token);
+
+        self.request_app_access_token().await
+    }
+
+    #[instrument(skip_all)]
+    async fn request_app_access_token(&self) -> AppResult<String> {
         let form = HashMap::from([
             ("client_id", self.twitch_config.client_id()),
             ("client_secret", self.twitch_config.client_secret()),
@@ -326,6 +331,21 @@ impl TwitchApi {
             .get(url)
             .bearer_auth(access_token)
             .header("Client-Id", self.twitch_config.client_id()))
+    }
+
+    #[instrument(skip_all)]
+    async fn send_with_retry_on_unauthorized(
+        &self,
+        request: RequestBuilder
+    ) -> Result<Response, Error> {
+        let mut response = request.send().await?;
+
+        if response.status() == StatusCode::UNAUTHORIZED {
+            let access_token = self.request_app_access_token().await?;
+            return request.bearer_auth(access_token).send().await;
+        }
+
+        return Ok(response)
     }
 }
 
